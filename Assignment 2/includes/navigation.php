1d88cae9ae1f6498af75178737c59d0d08764d8b
irfanbashirshah <35782195+irfanbashirshah@users.noreply.github.com>
2018-02-12 04:01:44 -0500

    Update navigation.php

--- Assignment 2/includes/navigation.php
+++ Assignment 2/includes/navigation.php
@@ -1,8 +1,11 @@
-<nav class = "menu">
-	<ul>
-		<li><a href="index.php">Home Page</a></li>
-		<li><a href="casey.php">Casey Brentnall</a></li>
-		<li><a href="irfan.php">Irfan Bashir</a></li>
-		<li><a href="jimmy.php">Jimm Yang</a></li>
-	</ul>
-</nav>
+<div data-collapse="medium" data-animation="default" data-duration="400" class="navbar w-nav">
+    <div class="w-container">
+      <nav role="navigation" class="w-nav-menu"><a href="index.php" class="w-nav-link">Home</a><a href="jimmy.php" class="w-nav-link">Jimmy</a><a href="casey.php" class="w-nav-link">Casey</a><a href="irfan.php" class="w-nav-link">Irfan</a></nav>
+      <div class="w-nav-button">
+        <div class="w-icon-nav-menu"></div>
+      </div>
+      <div class="div-block">Medellin Inc.</div>
+    </div>
+  </div>
+  <div class="container w-container"></div>
+  <div class="section"></div>
