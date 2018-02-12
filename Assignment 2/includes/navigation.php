2440b8b4da47045b63bdde77fe723c50ba2ac5dd
irfanbashirshah <35782195+irfanbashirshah@users.noreply.github.com>
2018-02-12 13:21:09 -0500

    Update navigation.php

--- Assignment 2/includes/navigation.php
+++ Assignment 2/includes/navigation.php
@@ -1,5 +1,5 @@
 <header>
-<div class = "logo"><h1><a href="index.php"><img src="images/logo.png" alt ="Logo"> 
+<div class = "logo"><h1><a href="index.php"><img src="images/da.jpg" alt ="Logo"> 
 Medellin Inc   </h1> </a> 
 CSC 174 - Advanced Front End Web Design </div>
 <hr>
