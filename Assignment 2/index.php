74a9c87b41fa77b2b09e0a7fe0552206fce6207f
irfanbashirshah <35782195+irfanbashirshah@users.noreply.github.com>
2018-02-12 03:32:26 -0500

    Update

--- Assignment 2/index.php
+++ Assignment 2/index.php
@@ -9,29 +9,20 @@
 		<link href="css/styles.css" rel="stylesheet">
 
 		<link href="css/navigation.css" rel="stylesheet">
-
-
-	</head>
-	<body>
-<?php include "includes/navigation.php" ?>
-
-
-		<p>This is the Assignment 2 Website of Team Medellín. Its members are Casey Brentnall, Irfan Bashir, and Jimmy Yang. Each member plays his own unique role in the website, and all contribute equally to it.</p>
-
-		<!-- Insert Javascript Plugin -->
-
-<script src="http://code.jquery.com/jquery.js"></script>
-<script src="js/menu-highlighter.js"></script>
-
-		<link rel="stylesheet" href="sss/sss.css">
+<link rel="stylesheet" href="sss/sss.css">
 		<link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
-		<script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-		<script src="sss/sss.min.js"></script>
+
+	
+
+
+
+		
+
+
 
 	</head>
 	<body>
-		
+		<?php include "includes/navigation.php" ?>
 		<div class="container">
 			<div class="slideshow">
 				<div class="slider">
@@ -43,7 +34,16 @@
 
 			<p>This is the Assignment 2 Website of Team Medellín. Its members are Casey Brentnall, Irfan Bashir, and Jimmy Yang. Each member plays his own unique role in the website, and all contribute equally to it.</p>
 			
-			<?php include "includes/navigation.php";?> 
+		
+			
+			
+			<script src="http://code.jquery.com/jquery.js"></script>
+<script src="js/menu-highlighter.js"></script>
+
+		
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+		<script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+		<script src="sss/sss.min.js"></script>
 			
 			<script> $('.slider').sss(); </script>
 		  		<script>
