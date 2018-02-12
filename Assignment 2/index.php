52582108058004234085f07565f239198231ee48
Irfan Bashir <irfanbashirshah@Github.com>
2018-02-12 03:22:38 -0500

    Update

--- Assignment 2/index.php
+++ Assignment 2/index.php
@@ -7,6 +7,7 @@
 
 		<title>Team Medellín, Assignment 2</title>
 		<link href="css/styles.css" rel="stylesheet">
+<<<<<<< HEAD
 		<link href="css/navigation.css" rel="stylesheet">
 
 
@@ -21,6 +22,39 @@
 
 <script src="http://code.jquery.com/jquery.js"></script>
 <script src="js/menu-highlighter.js"></script>
+=======
+		<link rel="stylesheet" href="sss/sss.css">
+		<link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+		<script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+		<script src="sss/sss.min.js"></script>
+
+	</head>
+	<body>
+		
+		<div class="container">
+			<div class="slideshow">
+				<div class="slider">
+				 	<img src="images/casey.jpg" alt="Casey">
+				 	<img src="images/irfan.png" alt="Irfan">
+				 	<img src="images/jimmy.jpg" alt="Jimmy">
+				</div><!--.slider-->
+			</div><!--.slideshow-->
+
+			<p>This is the Assignment 2 Website of Team Medellín. Its members are Casey Brentnall, Irfan Bashir, and Jimmy Yang. Each member plays his own unique role in the website, and all contribute equally to it.</p>
+			
+			<?php include "includes/navigation.php";?> 
+			
+			<script> $('.slider').sss(); </script>
+		  		<script>
+				 	$( "#accordion" ).accordion({
+				 		heightStyle: "content"
+
+				 	});
+			</script>
+		</div><!--.container-->
+
+>>>>>>> d0e90370254c65a8d0b02eb10c8cb2bd71d5cc47
 	</body>
 
 </html>
