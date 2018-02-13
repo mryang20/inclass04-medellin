528cc8c12980a238340d4876fb7fcf26357b8639
mryang20 <35783113+mryang20@users.noreply.github.com>
2018-02-13 16:06:45 -0500

    Update casey.php

--- Assignment 2/casey.php
+++ Assignment 2/casey.php
@@ -21,25 +21,22 @@
 		<h2>About Me</h2>
 
 			<h3>Introduction</h3>
-
 				<p>Hello! My name is Casey Adam Brentnall. I am a sophomore, and my major is computer science. I was born on January 12, 1998, and my hometown is Rochester, NY.</p>
 
 			<h3>Work Experience</h3>
+				<p>Currently, I work as a Standardized Patient in a Suicide Prevention Training program at URMC. In this program, I've developed a character who is at risk for suicide, and I work with physicians and psychologists, helping them learn how to identify and treat depression and suicide. In the past, I've worked as a cashier at BJ's Wholesale Club, and Family Dollar. At these jobs, I both ran a cash register and stocked, cleaned, and handled returns. I've also worked summers as an ice cream scooper and cook at Friendly's. I learned how to make a variety of simple food items, and was in charge of cleaning the grill area thoroughly at the end of every shift.</p>
 
-				<p>My first job was at BJ’s Wholesale Club, near Eastview Mall. I was a cashier, and I worked there the summer between Junior and Senior year. I worked there for about 7 months, but during the winter of Senior year, working was taking its toll on my schoolwork and college search. I quit without giving two weeks’ notice, so I never write them as a reference when I apply for jobs.</p>
+			<h3>Course Work</h3>
+				<p>When I enrolled at the University of Rochester, I had planned on pursuing a double major in Computer Science and English. Because of this, my previous coursework is a pretty even mix of CSC and ENG courses. I've taken classes in acting, directing, dialect training, and in theatrical performance. On the computer science side, I've taken Data Structures and Algorithms, Formal Computation and Systems, and all the required math courses. This semester, I'm taking Introduction to Artificial Intelligence, my next step in the computer science track. I'm also taking Linear Algebra and Differential Equations, my first math class after calculus. I'm taking this class, and lastly I'm taking Musical Theatre Workshop, a course in preparing scenework and audition materials for musical theatre actors.</p>
 
-				<p>The next summer, before college, I worked at Family Dollar in Farmington. I applied for this job because I thought that, being a smaller store, it would be faster paced than my job at BJ’s, where I spent hours standing with no customers checking out, but unable to do anything other than stay in my aisle. At Family Dollar, I was a cashier, but was also in charge of stocking, cleaning, and to a certain extent, stopping theft.</p>
-
-				<p>The summer between my first and second semester of college, I worked at Friendly’s in Eastview Mall. Being a broke college student, this was the summer job that I worked the most hours at. I had applied for a couple of high-paying internships at computer science companies, but never heard back from them, so I tried to make up for it by working longer hours. This mall location of Friendly’s is an “express” location, where there are no waiters: orders are taken at the ice cream counter in the front, and then food is taken from the kitchen to the customers, who seat themselves. The POS system at Friendly’s was by far the most complicated in my experiences as a cashier, and it was actually pretty difficult to learn how to make each ice cream dish. The milkshake machines didn’t work. Everything got sticky. The food preparation areas needed to be spotless. Once a week, our ice cream scoops would be weighed to make sure that we weren’t over-serving.</p> 
-
-				<p>After a while working there, the General Manager asked me to train to work on the grill. A week and many burns later, I had learned how to man the grill. This work was hotter, much faster paced, and more physically and mentally intensive, but I made over a dollar more an hour than in the front. The management, however, was very rude at this location, and sometimes my safety concerns or personal issues weren’t taken seriously, so I left about 2 weeks before heading back to college. I don’t intend to return to the job next summer.</p>
+			<h3>Hobbies and Interests</h3>
+				<p>In my free time, I like to sing, play guitar, and see as many movies as I can. On campus, I'm involved in TOOP, a student run theatrical group, and ROC Players, a musical theatre organization. In the past, I've appeared in plays in the Todd International Theatre Program. This semester, I'll be appearing onstage in ROC Player's production of Spring Awakening as Moritz Steifel.</p>
 
 			<h3>Hometown Info</h3>
 				<p>I was born in Farmington, New York. Farmington is a town located in the northern part of Ontario County, New York, United States. The population was 11,825 at the 2010 census. Farmington is about twenty-five miles southeast of Rochester, New York. The town was formed along with its county in 1789. The town is governed by a town council, and the council's supervisor is Peter Ingalsbe.</p>
-	
-			<h3>Initial Role</h3>
-				<p><strong> My initial role was to be a design artist. I would be responsible for everything the user sees, in any browser type and size. I make decisions about the graphic design, specifically relating to the colors, the styles, the fonts, and the layouts of the website. I would ensure these components would work across any browser type and size. For me, good graphic design shouldn't cause readability problems. The presentation layer (styles) would be coded in CSS so it is visually arranged in a way that makes sense for the content and adds value to the structure layer. Styles (CSS) should meet industry standards and best practices including W3C CSS validation within reason. </strong></p>
 
+			<h3>Initial Role</h3>
+				<p>My initial role was to be a design artist. I would be responsible for everything the user sees, in any browser type and size. I make decisions about the graphic design, specifically relating to the colors, the styles, the fonts, and the layouts of the website. I would ensure these components would work across any browser type and size. For me, good graphic design shouldn't cause readability problems. The presentation layer (styles) would be coded in CSS so it is visually arranged in a way that makes sense for the content and adds value to the structure layer. Styles (CSS) should meet industry standards and best practices including W3C CSS validation within reason.</p>
 		</article>
 		
 <script src="http://code.jquery.com/jquery.js"></script>
