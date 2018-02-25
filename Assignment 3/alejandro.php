8c7be3421021fe137b7ee2f391a15e9e448286b1
cbrentna <35782605+cbrentna@users.noreply.github.com>
2018-02-25 17:14:18 -0500

    website outline created

---
+++ Assignment 3/alejandro.php
@@ -0,0 +1,40 @@
+	<?php include "inc/html-top.inc" ?>
+
+	<!-- I've already assigned a class called "menu" to the nav element, to use with CSS. -->
+	<?php include "inc/nav.inc" ?>
+	<body>
+		<div class="container">
+			<header>
+				<!-- For the h1's on this page, I was thinking of using a "coding" type font. A good choice would be Inconsolata, its on google fonts. Maybe use the same font for all the "titles", h1, h2, etc -->
+				<h1>CS Majors in CSC 174</h1>
+				<!-- I've already assigned a class called "menu" to the nav element, to use with CSS. -->
+				<?php include "inc/nav.inc" ?>
+			</header>
+
+			<article>
+				<h2>Alejandro Abreu</h2>
+
+				<h3>About Me</h3>
+
+					<p>My name is Alejandro Abreu, and I was born on September 6th, 1995. I was born in Monte Sinai hospital in Manhattan, and a few months later moved to the Bronx where I currently reside. I am from New York City, more specifically, the Bronx. As a youth, I was very active and in love with sports.</p>
+
+					<p>I spent most of my time at the park or at the gym either playing baseball or basketball. I attended the P.S 220 for elementary school and later moved on the school for Applied Mathematics and Technology, AAMT, for middle school. At AAMT, I played basketball and ran track for the school team. After completing middle school, I moved on and attended high school at A Philip Randolph High School Campus. In high school, I was rejected from joining the varsity baseball team which led me to pursue other sports. This led me to playing Varsity Lacrosse and Bowling.</p>
+
+					<p>I graduated from A Philip Randolph in Spring 2013. I was accepted to various university but none call out to me like the one that I currently attend. At the moment, I am a senior at the University of Rochester pursuing a degree in Computer Science. I am anticipated to graduate from the University of Rochester in May 2018. In my lifetime I have worked in about 5 different location varying from a gym to a school.</p>
+
+				<h3>Work Experience</h3>
+
+				<p>Starting in 2014, I began working as a building manager at Goergen Athletic Center here on campus. I worked on making sure the building was running smoothly. Before that, I worked as a camp counselor for two years at 92Y Camps, where I was in charge of making sure the campers were all safe and having an awesome time. Before that, I worked as a tutor in mathematics for Upward Bound, where I helped kids improve their math skills as well as prepare for the SAT.</p>
+
+			</article>
+
+			<aside>
+				<figure>
+					<img src="images/alejandro.jpg" alt="Alejandro Abreu">
+					<figcaption>Senior CS student Alejandro Abreu</figcaption>
+				</figure>
+			</aside>
+			<?php include "inc/foot.inc" ?>
+		</div><!-- .container -->
+	</body>
+</html>
---
+++ Assignment 3/zubair.php
@@ -0,0 +1,39 @@
+	<?php include "inc/html-top.inc" ?>
+	<body>
+		<div class="container">
+			<header>
+				<!-- For the h1's on this page, I was thinking of using a "coding" type font. A good choice would be Inconsolata, its on google fonts. Maybe use the same font for all the "titles", h1, h2, etc -->
+				<h1>CS Majors in CSC 174</h1>
+				<!-- I've already assigned a class called "menu" to the nav element, to use with CSS. -->
+				<?php include "inc/nav.inc" ?>
+			</header>
+			<article>
+				<h2>Zubair Aziz</h2>
+
+				<h3>About Me</h3>
+
+					<p>I am currently a junior, anticipated to graduate in May, 2019. I started off my first year in college majoring in Economics. I had the privilege to enroll in some interesting courses in the department, such as Sports Economics, where we tested out various economic theories in a sports setting, and Econometrics where students are taught how to use the tools that enable us to extract useful information about important economic policy from a set of available data.</p>
+
+					<p>I also decided to major in Computer Science after taking a few computer science courses that seemed interesting to me. Because of this, towards the end of my college “career”, I’m taking mostly CS courses. However, this semester in particular will be very interesting as I’m also enrolled in a dance class and an art class. I'm excited for this chance to branch out and obtain new experiences before I graduate.</p>
+
+					<p>I'm involved in some extra-curricular activities such as Alpha Kappa Psi, the professional business fraternity; and Grass Roots Soccer, a health organization that uses the power of soccer to educate, inspire, and mobilize young people to lead healthier lives. These two organizations have helped me develop so much as a student, where Alpha Kappa Psi has helped me develop my professional skills tremendously, and Grass Roots Soccer has given me a great opportunity to give back to the community.I enjoy painting in my free time. I took up painting a few years ago, teaching myself different painting techniques on various mediums. I mostly do nature scenes and landscapes. Attempting these types of paintings tend to help relax and find solace in the quiet and solidarity I find while painting.</p>
+
+				<h3>Work Experience</h3>
+
+				<p>I currently work for IT Services on campus, working as an IT Consultant. Here, I assist with network registration and troubleshoot network issues, troubleshoot NetID and login issues, provide support through various methods,assist with various types of malware on students' personal computers. I’d say that the most interesting experience while working here are the night-owl shifts, where I work at the help desk, doing the exact same thing an IT Consultant would normally do, except at the late hours of midnight to 8 am.</p>
+
+				<p>Other than that, last summer I had the chance to work for Media Prima Digital, a multimedia company based in Malaysia. There, I was an intern working as a developer that worked on web & app development. Here, I worked with the team to develop modern websites and apps for various different uses. I was also trained in both front end and back end systems for web development. I learned how to optimize pages for better load speeds and better rankings for search engine optimization. One of the more notable projects we had was working with Robot Framework, a Python extension, to automate web page testing.</p>
+
+			</article>
+
+			<aside>
+				<!-- If you want to do a slideshow on the index page, zubair's picture will probably have to be cropped. The other two are already the same size. -->
+				<figure>
+					<img src="images/zubair.jpg" alt="Zubair Aziz">
+					<figcaption>Junior CS student Zubair Aziz</figcaption>
+				</figure>
+			</aside>
+			<?php include "inc/foot.inc" ?>
+		</div><!-- .container -->
+	</body>
+</html>
