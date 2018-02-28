6c6702a4d524d5409d97f62503bc11eb2814b798
mryang20 <35783113+mryang20@users.noreply.github.com>
2018-02-28 14:33:53 -0500

    Update zubair.php

--- Assignment 3/zubair.php
+++ Assignment 3/zubair.php
@@ -38,5 +38,8 @@
 			</article>
 			
 			<?php include "inc/foot.inc" ?>
+		
+			<script src="http://code.jquery.com/jquery.js"></script>
+			<script src="js/menu-highlighter.js"></script>
 	</body>
 </html>
